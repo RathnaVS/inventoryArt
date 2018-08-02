3929632d2fe5709bc5bb061c9575070a0a3ba61b
RathnaVS <38005977+RathnaVS@users.noreply.github.com>
2018-08-02 12:33:33 +0530

    Second class

---
+++ src/main/java/inventoryArt/App1.java
@@ -0,0 +1,12 @@
+package inventoryArt;
+
+public class App1 {
+
+	public static void main(String[] args){
+		System.out.println("Hello Ramesh and Neha");
+	}
+
+	public int calc1(){
+		return 0;
+	}
+}
---
+++ src/test/java/inventoryArt/App1Test.java
@@ -0,0 +1,15 @@
+package inventoryArt;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class App1Test {
+	
+	@Test
+	public void testApp()
+	{
+		assertEquals(0,new App1().calc1());
+	}
+
+	
+}
